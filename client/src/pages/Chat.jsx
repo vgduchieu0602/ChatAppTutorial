9324299be82816c25b082506d6673a203d374a32
vgdhieu0602 <vg.dhieu0602@gmail.com>
2024-11-13 05:11:26 +0700

    finish fetch chat

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,10 +1,15 @@
+import { useContext } from 'react';
 import Navbar from '../components/Navbar'
+import { ChatContext } from '../context/ChatContext';
 
 const Chat = () => {
+    const {userChats, isUserChatsLoading, userChatsError} = useContext(ChatContext)
+
+    console.log('UserChats: ', userChats)
     return ( 
         <>
             <Navbar>
-
+                <h1>Chat app</h1>
             </Navbar>
         </>
      );
